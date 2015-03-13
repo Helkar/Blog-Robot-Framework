89d42524066c2fdba12813e65520025dc0b82cff
Victor <sirhelkar@gmail.com>
2015-03-13 01:29:21 +0100

    Now when there are more than one post with the same title and body, the most updated is returned

--- database/rails_database.py
+++ database/rails_database.py
@@ -7,15 +7,19 @@ RAILS_DB = os.path.join(os.environ['HOME'], 'RoR', 'blog', 'db', 'development.sq
 def get_post_id(title, body):
     db = sqlite3.connect(RAILS_DB)
     cursor = db.cursor()
-    cursor.execute('SELECT * FROM posts')
+    cursor.execute('SELECT * FROM posts where title=? and body=? order by created_at DESC', (title,body))
     all_rows = cursor.fetchall()
-    for row in all_rows:
-        post_id = row[0]
-        post_title = row[1]
-        post_body = row[2]
-        if post_title == title and post_body == body:
-            return post_id
-    return None
+    if len(all_rows) > 1:
+        created = all_rows[0][3]
+        post_id = all_rows[0][0]
+        for row in all_rows:
+            if row[4] > created:
+                post_id = row[0]
+                created = row[0]
+        return post_id
+    else:
+        return all_rows[0][0]
+    
 
 def main():
     title = sys.argv[1]
